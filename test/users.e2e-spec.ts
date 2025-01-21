6eda58ced5a1c63a4d0211501fc528efa501d62e
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2025-01-21 15:31:08 +0400

    add config for tests

---
+++ test/users.e2e-spec.ts
@@ -0,0 +1,7 @@
+describe('integration tests for users', () => {
+  describe('createUser', () => {
+    it('should be able to create user', async () => {
+      expect.assertions(2);
+    });
+  });
+});
