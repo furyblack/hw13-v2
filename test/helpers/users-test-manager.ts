a3ba5305862e293279cf07629342bafcb3008336
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2025-01-22 17:31:41 +0400

    add tests get and create users

--- test/helpers/users-test-manager.ts
+++ test/helpers/users-test-manager.ts
@@ -2,6 +2,7 @@ import { HttpStatus, INestApplication } from '@nestjs/common';
 import { CreateUserInputDto } from '../../src/moduls/user-accounts/api/input-dto/users.input-dto';
 import { UserViewDto } from '../../src/moduls/user-accounts/api/view-dto/users.view-dto';
 import request from 'supertest';
+import { delay } from './delay';
 
 export class UsersTestManager {
   constructor(private app: INestApplication) {}
@@ -14,5 +15,19 @@ export class UsersTestManager {
       .send(createModel)
       .expect(statusCode);
     return responce.body;
+  }
+  async createSeveralUsers(count: number): Promise<UserViewDto[]> {
+    const usersPromises = [] as Promise<UserViewDto>[];
+
+    for (let i = 0; i < count; ++i) {
+      await delay(50);
+      const response = this.createUser({
+        login: `test` + i,
+        email: `test${i}@gmail.com`,
+        password: '123456789',
+      });
+      usersPromises.push(response);
+    }
+    return Promise.all(usersPromises);
   }
 }
--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,9 +1,12 @@
 import { CreateUserDto } from '../src/moduls/user-accounts/dto/create-user.dto';
 import { UsersTestManager } from './helpers/users-test-manager';
 import { JwtService } from '@nestjs/jwt';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { initSettings } from './helpers/init-settings';
 import { deleteAllData } from './helpers/delete-all-data';
+import request from 'supertest';
+import { PaginatedViewDto } from '../src/core/dto/base.paginated.view-dto';
+import { UserViewDto } from '../src/moduls/user-accounts/api/view-dto/users.view-dto';
 
 describe('users', () => {
   let app: INestApplication;
@@ -42,5 +45,18 @@ describe('users', () => {
       id: expect.any(String),
       createdAt: expect.any(String),
     });
+  });
+  it('should get users with paging', async () => {
+    const users = await userTestManger.createSeveralUsers(12);
+    const { body: responseBody } = (await request(app.getHttpServer())
+      .get(`/api/users?pageNumber=2&sortDirection=asc`)
+      .expect(HttpStatus.OK)) as { body: PaginatedViewDto<UserViewDto> };
+
+    expect(responseBody.totalCount).toBe(12);
+    expect(responseBody.items).toHaveLength(2);
+    expect(responseBody.pagesCount).toBe(2);
+    //asc sorting
+    expect(responseBody.items[1]).toEqual(users[users.length - 1]);
+    //etc...
   });
 });
